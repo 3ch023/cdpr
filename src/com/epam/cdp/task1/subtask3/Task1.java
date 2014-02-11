48d914673df50bf8ab21ba2b4f9e3445f74e10c2
Mariia_Lukianets <Mariia_Lukianets@epam.com>
2014-02-11 14:45:12 +0200

    Fixes

--- src/com/epam/cdp/task1/subtask3/Task1.java
+++ src/com/epam/cdp/task1/subtask3/Task1.java
@@ -1,8 +1,6 @@
 package com.epam.cdp.task1.subtask3;
 
-import com.epam.cdp.task1.subtask3.storage.HashMapStorage;
-import com.epam.cdp.task1.subtask3.storage.Storage;
-import com.epam.cdp.task1.subtask3.storage.TreeMapStorage;
+import com.epam.cdp.task1.subtask3.storage.*;
 import sun.misc.Regexp;
 
 import java.io.BufferedReader;
@@ -15,54 +13,62 @@ import java.util.regex.Pattern;
  * Created by Mariia_Lukianets on 05.02.14.
  */
 public class Task1 {
+    static Pattern pattern = Pattern.compile("([a-zA-Z]+)(.*)");
+
     public static void main(String[] args) throws IOException {
 
-        Pattern pattern = Pattern.compile("([a-zA-Z]+)(.*)");
+        String fileName = "wap.txt";
 
-        BufferedReader br = new BufferedReader(new FileReader("wap.txt"));
-        String line = br.readLine();
 
         Storage hashMapStorage = new HashMapStorage();
+        System.out.println("HashMap");
+        testStorage(hashMapStorage, fileName);
+
         Storage treeMapStorage = new TreeMapStorage();
+        System.out.println("TreeMap");
+        testStorage(treeMapStorage, fileName);
+
+        Storage linkedListStorage = new LinkedListStorage();
+        System.out.println("LinkedList");
+        testStorage(linkedListStorage, fileName);
+
+        Storage arrayListStorage = new ArrayListStorage();
+        System.out.println("ArrayList");
+        testStorage(arrayListStorage, fileName);
+
+    }
+
+    static public void testStorage(Storage storage, String fileName) throws IOException {
+        int entryCount, uniqueWordsCount;
+        long start, end;
+        BufferedReader br = new BufferedReader(new FileReader(fileName));
+        String line = br.readLine();
         String[] words;
         Matcher matcher;
 
+        start = System.currentTimeMillis();
         while (line != null) {
             words = line.split(" ");
             for(String word : words) {
                 matcher = pattern.matcher(word);
                 if(matcher.matches()) {
                     word = matcher.group(1);
-
-                    hashMapStorage.add(word.toLowerCase());
-                    treeMapStorage.add(word.toLowerCase());
+                    storage.add(word.toLowerCase());
                 }
             }
             line = br.readLine();
         }
+        end = System.currentTimeMillis();
+        System.out.println("Initialization time: " + (end - start));
 
-        test(hashMapStorage);
-        test(treeMapStorage);
 
-    }
+        start = System.currentTimeMillis();
+        entryCount = storage.entryCount("love");
+        uniqueWordsCount = storage.uniqueWordsCount();
+        end = System.currentTimeMillis();
 
-    static public void test(Storage storage) {
-        int entryCount;
-        int uniqueWordsCount;
-        long start;
-        long end;
-
-        for(int i = 0; i < 3; i++) {
-            start = System.currentTimeMillis();
-
-            entryCount = storage.entryCount("love");
-            uniqueWordsCount = storage.uniqueWordsCount();
-
-            end = System.currentTimeMillis();
-
-            System.out.println(i + "Time: " + (end - start));
-
-        }
+        System.out.println("Methods time: " + (end - start));
+        System.out.println("Entry count: " + entryCount + ", unique words: " + uniqueWordsCount);
         System.out.println();
     }
 }
--- src/com/epam/cdp/task1/subtask3/storage/MapStorage.java
+++ src/com/epam/cdp/task1/subtask3/storage/MapStorage.java
@@ -30,35 +30,3 @@ public abstract class MapStorage implements Storage {
         return count;
     }
 }
-package com.epam.cdp.task1.subtask3.storage;
-
-        import java.util.Map;
-
-/**
- * Created by Mariia_Lukianets on 07.02.14.
- */
-public abstract class MapStorage implements Storage {
-    Map<String, Integer> storage;
-
-    @Override
-    public void add(String word) {
-        Integer entryCount = storage.get(word) == null ? 0 : storage.get(word);
-        storage.put(word, ++entryCount);
-    }
-
-    @Override
-    public int entryCount(String word) {
-        return storage.get(word) == null ? 0 : storage.get(word);
-    }
-
-    @Override
-    public int uniqueWordsCount() {
-        int count = 0;
-        for(Map.Entry<String, Integer> entry : storage.entrySet()) {
-            if(entry.getValue() == 1) {
-                count ++;
-            }
-        }
-        return count;
-    }
-}
